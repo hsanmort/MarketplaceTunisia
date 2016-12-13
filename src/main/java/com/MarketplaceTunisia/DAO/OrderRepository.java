117a7dad280af7e4f30e75ae2aaddc603693ac6b
naoufel <naoufel.bensalem@sesame.com.tn>
2016-12-13 21:20:09 +0100

    Repository Order

--- src/main/java/com/MarketplaceTunisia/DAO/OrderRepository.java
+++ src/main/java/com/MarketplaceTunisia/DAO/OrderRepository.java
@@ -1,5 +1,20 @@
 package com.MarketplaceTunisia.DAO;
 
-public interface OrderRepository {
+import java.util.Date;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+import com.MarketplaceTunisia.Entities.Order;
+
+
+
+public interface OrderRepository extends JpaRepository<Order, Integer>{
+
+	
+	public Page<Order> findBydate(Date date0, Pageable pageable);
+	//public Page<Order> findAll(Pageable p);
 }
