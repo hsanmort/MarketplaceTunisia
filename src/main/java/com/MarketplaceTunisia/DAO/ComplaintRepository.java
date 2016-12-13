872722a24da77b37a913e96193ad4541d0a409c2
haythemgara <haythem.gara@gmail.com>
2016-12-13 19:00:37 +0100

    hhh

--- src/main/java/com/MarketplaceTunisia/DAO/ComplaintRepository.java
+++ src/main/java/com/MarketplaceTunisia/DAO/ComplaintRepository.java
@@ -1,9 +1,21 @@
 package com.MarketplaceTunisia.DAO;
 
+import java.awt.print.Pageable;
+import java.util.Date;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.MarketplaceTunisia.Entities.Complaint;
 
+
+
 public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
 
+	@Query("select c from Complaint c where c.description like :x")
+	public Page<Complaint> findByMc(@Param("x") String mc, Pageable pageable);
+	public Page<Complaint> findBydateComplaint(Date date, Pageable pageable);
+	
 }
--- src/main/java/com/MarketplaceTunisia/DAO/ProductRepository.java
+++ src/main/java/com/MarketplaceTunisia/DAO/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.MarketplaceTunisia.DAO;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.awt.print.Pageable;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.MarketplaceTunisia.Entities.Category;
 import com.MarketplaceTunisia.Entities.Product;
+import com.MarketplaceTunisia.Entities.Shop;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+	
+	@Query("select p from Product p where p.nameProduct like :x or p.description like :x")
+	public Page<Product> findByMc(@Param("x") String mc, Pageable pageable);
+	public Page<Product> findByCategory(Category category, Pageable pageable);
+	public Page<Product> findByShop(Shop shop, Pageable pageable);
 }
