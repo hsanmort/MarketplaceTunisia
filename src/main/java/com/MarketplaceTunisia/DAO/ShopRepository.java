d515d06072fa2de8a7a24c75e2aa63e48638ce1f
haythemgara <haythem.gara@gmail.com>
2016-12-13 20:51:59 +0100

    service1

--- src/main/java/com/MarketplaceTunisia/DAO/ShopRepository.java
+++ src/main/java/com/MarketplaceTunisia/DAO/ShopRepository.java
@@ -1,11 +1,15 @@
 package com.MarketplaceTunisia.DAO;
-import java.awt.print.Pageable;
+
 import java.util.Date;
 
 import com.MarketplaceTunisia.Entities.Seller;
 import com.MarketplaceTunisia.Entities.Shop;
 
+
+
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +18,6 @@ public interface ShopRepository extends JpaRepository<Shop, Integer>{
 
 	@Query("select s from Shop s where s.description like :x or s.nameShop like :x")
 	public Page<Shop> findByMc(@Param("x") String mc, Pageable pageable);
-	
 	public Page<Shop> findBynameShop(String nameShop, Pageable pageable);
 	public Page<Shop> findBydateCreation(Date dateCreation, Pageable pageable);
 	public Page<Shop> findByseller(Seller seller, Pageable pageable);
---
+++ src/main/java/com/MarketplaceTunisia/Services/AdressService.java
@@ -0,0 +1,29 @@
+package com.MarketplaceTunisia.Services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.MarketplaceTunisia.DAO.AdressRepository;
+import com.MarketplaceTunisia.Entities.Adress;
+
+@RestController
+public class AdressService {
+
+	@Autowired
+	AdressRepository adressRepository;
+	
+	@RequestMapping(value="/adress/save",method=RequestMethod.POST)
+	public void saveAdress(@RequestBody Adress adress){
+		adressRepository.save(adress);
+	}
+	
+	@RequestMapping(value="/adress/all", method=RequestMethod.GET)
+	public List<Adress> liste() {
+		return adressRepository.findAll();
+	}
+}
