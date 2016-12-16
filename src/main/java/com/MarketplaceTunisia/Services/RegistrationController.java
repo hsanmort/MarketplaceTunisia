3ba63a4a56f291d10a0115c38d84f8fcae3cc696
haythemgara <haythem.gara@gmail.com>
2016-12-16 11:35:08 +0100

    adding (seller+buyer) with roles done

--- src/main/java/com/MarketplaceTunisia/Services/RegistrationController.java
+++ src/main/java/com/MarketplaceTunisia/Services/RegistrationController.java
@@ -3,7 +3,6 @@ package com.MarketplaceTunisia.Services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.MarketplaceTunisia.DAO.RoleRepository;
 import com.MarketplaceTunisia.DAO.UserRepository;
-
+import com.MarketplaceTunisia.Entities.Buyer;
 import com.MarketplaceTunisia.Entities.Role;
+import com.MarketplaceTunisia.Entities.Seller;
 import com.MarketplaceTunisia.Entities.User;
 
 @RestController
@@ -26,17 +26,21 @@ public class RegistrationController {
 	private RoleRepository roleRepository;
 	
 	@RequestMapping(value="/user/buyerReg",method=RequestMethod.POST)
-	public void RegistrationBuyer(@RequestBody User u){
-		
+	public void RegistrationBuyer(@RequestBody Buyer u){
+		Role r2=roleRepository.findOne("INVITE");
 		Role r=roleRepository.findOne("BUYER");
 		u.getRoles().add(r);
+		u.getRoles().add(r2);
+
 		userRepository.save(u);
 	}
 	@RequestMapping(value="/user/SellerReg",method=RequestMethod.POST)
-	public void RegistrationSeller(@RequestBody User u){
-		
+	public void RegistrationSeller(@RequestBody Seller u){
+		Role r2=roleRepository.findOne("INVITE");
 		Role r=roleRepository.findOne("SELLER");
 		u.getRoles().add(r);
+		u.getRoles().add(r2);
+
 		userRepository.save(u);
 	}
 	
