e55edbbb0b73500f4bf2814eabd0611157556ef6
haythemgara <haythem.gara@gmail.com>
2016-12-23 20:28:57 +0100

    with one controller App.js
    registration in progress

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -91,15 +91,12 @@
 // js/app.js
 'use strict';
 
-
 /**
  * Déclaration de l'application routeApp
  */
 var routeApp = angular.module('routeApp', [
-    // Dépendances du "module"
-    'ngRoute',
-    'routeAppControllers'
-]);
+// Dépendances du "module"
+'ngRoute', 'routeAppControllers' ]);
 
 /**
  * Configuration du module principal : routeApp
@@ -107,48 +104,84 @@ var routeApp = angular.module('routeApp', [
 //routeApp.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider','$compileProvider', '$filterProvider', '$provide', '$httpProvider',
 //    function($routeProvider, routeResolverProvider, $controllerProvider,
 //          $compileProvider, $filterProvider, $provide, $httpProvider) { 
-routeApp.config(['$routeProvider',
-                 function($routeProvider) { 
-        // Système de routage
-        $routeProvider
-        .when('/home', {
-            templateUrl: 'views/home.html',
-            controller: 'homeCtrl'
-        })
-        .when('/vendors', {
-            templateUrl: 'views/vendors/vendors.html',
-            controller: 'vendorsController'
-            //resolve: resolveController('/controllers/vendorsController.js')
-        })
-        .when('/RegBuyer', {
-            templateUrl: 'views/security/Account_login_Buyer.html',
-            controller: '/controllers/registerController.js'
-        })
-        .when('/RegSeller', {
-            templateUrl: 'views/security/Account_login_Seller.html',
-            controller: '/controllers/registerController.js'
-        })
-       
-        .otherwise({
-            redirectTo: '/home'
-        });
-    }
-]);
+routeApp.config([ '$routeProvider', function($routeProvider) {
+	// Système de routage
+	$routeProvider.when('/home', {
+		templateUrl : 'views/home.html',
+		controller : 'homeCtrl'
+	}).when('/vendors', {
+		templateUrl : 'views/vendors/vendors.html',
+		controller : 'vendorsController'
+	//resolve: resolveController('/controllers/vendorsController.js')
+	}).when('/RegBuyer', {
+		templateUrl : 'views/security/Account_login_Buyer.html',
+		controller : 'registerBuyerController',
+		controllerAs: 'vm'	
+			
+	}).when('/RegSeller', {
+		templateUrl : 'views/security/Account_login_Seller.html',
+		controller : 'registerSellerController'
+	})
 
-
+	.otherwise({
+		redirectTo : '/home'
+	});
+} ]);
 
 /**
  * Définition des contrôleurs
  */
 var routeAppControllers = angular.module('routeAppControllers', []);
 
-
 // Contrôleur de la page d'accueil
-routeAppControllers.controller('homeCtrl', ['$scope',
-    function($scope){
-        $scope.message = "Bienvenue sur la page d'accueil";
-    }
-]);
+routeAppControllers.controller('homeCtrl', [ '$scope', function($scope) {
+	$scope.message = "Bienvenue sur la page d'accueil";
+} ]);
 
+
+//controlleur inscription buyer & seller
+routeAppControllers.controller('registerBuyerController', [ '$scope',
+		function($scope) {
+			$scope.message = "Bienvenue sur la page du Registration Du Buyer";
+		} ]);
+routeAppControllers.controller('registerSellerController', [ '$scope',
+		function($scope) {
+			$scope.message = "Bienvenue sur la page du Registration Du Seller";
+		} ]);
+//registration buyer
+routeAppControllers.$inject = ['RegistrationController', '$location', '$rootScope'];
+function routeAppControllers(RegistrationController, $location, $rootScope) {
+    var vm = this;
+
+    vm.register = register;
+
+    function register() {
+        vm.dataLoading = true;
+        RegistrationController.RegistrationBuyer(vm.buyer)
+            .then(function (response) {
+                if (response.success) {
+                    $location.path('/home');
+                } else {
+                    vm.dataLoading = false;
+                }
+            });
+    }
+}
+
+
+//Contrôleur de la page vendors
+routeAppControllers.controller('vendorsController', [ '$scope', '$http',
+		function($scope, $http) {
+			$scope.users = [];
+			$scope.message = "Bienvenue sur la page du vendeur";
+
+			function chargerAll() {
+				$http.get("/user/all").success(function(data) {
+					$scope.users = data;
+				});
+			}
+			;
+			chargerAll();
+		} ]);
 
 
