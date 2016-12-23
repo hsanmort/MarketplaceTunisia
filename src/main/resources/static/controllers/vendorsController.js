a58f94057f23da94d50343c1177b884c7bb3bf40
hsanmort <hassan.akrout@gmail.com>
2016-12-23 23:09:05 +0100

    test

--- src/main/resources/static/controllers/vendorsController.js
+++ src/main/resources/static/controllers/vendorsController.js
@@ -16,3 +16,21 @@ function($scope,$http){
    chargerAll();
 }
 ]);
+//(function () {
+//    'use strict';
+// 
+//    var routeApp= angular.module('routeApp');
+//    routeApp.controller('vendorsController', ['$scope','$http', 
+//    		function ($scope,$http) {
+//    	$scope.users=[];
+//      $scope.message = "Bienvenue sur la page du vendeur";
+//    	function chargerAll(){
+//       	$http.get("/user/all")
+//       	.success(function(data) {
+//       		$scope.users=data;	
+//       	});
+//       };
+//       chargerAll();
+//    }]);
+// 
+//}());
