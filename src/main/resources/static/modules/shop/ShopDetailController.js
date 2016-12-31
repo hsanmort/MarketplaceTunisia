3311baf7d21ee2f8de0176ab413be476b794d4cc
hsanmort <hassan.akrout@gmail.com>
2017-01-01 00:58:14 +0100

    modif

--- src/main/resources/static/modules/shop/ShopDetailController.js
+++ src/main/resources/static/modules/shop/ShopDetailController.js
@@ -4,17 +4,36 @@ angular.module('Shop')
 .controller('ShopDetailController',
 		    ['$scope','$http','$location','$routeParams',
 		        function ($scope, $http, $location,$routeParams) {
-		    	var idShop = $routeParams.id;
-		      $scope.shopbyid=[];
-		    	 $scope.message = idShop+" Shop Here";
+		    	$scope.idShop=null;
+		    	$scope.idShop  = $routeParams.id;
+		      $scope.shopbyid={};
+		    	 $scope.message = $scope.idShop +" Shop Here";
 		    	 
-		    	 function detailShop(shop){
+		    	 function detailShop(){
 		    		 
-		    		 $http.get("/shop/findByIdShop?idShop="+idShop)
+		    		 $http.get("/shop/findByIdShop?idShop="+$scope.idShop)
 			  	      .success(function(data) {
 			  	         $scope.shopbyid=data;   
+			  	         
 			  	      });
 		    	   };
 		    	   detailShop();
+		    	  
+//		    	   var Shop = $resource("/shop/findByIdShop?idShop="+$scope.idShop,
+//		    			    {
+//		    			    charge: {method:'GET', params:{charge:true}}
+//		    			   });
+//		    	   console.log( Shop);
+		    	   
+		    	   $scope.productbyshop={};
+		    	   function ProductByShop(){
+			    		 
+			    		 $http.get("/product/findByShop?shop="+ $scope.shopbyid)
+				  	      .success(function(data) {
+				  	         $scope.productbyshop=data;
+				  	         console.log( $scope.productbyshop);
+				  	      });
+			    	   };
+			    	   ProductByShop();
   
 		     }]);
